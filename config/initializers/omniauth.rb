5b127b463e3fb0f47db7cf1f76d75135979ebe5e
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-12 04:06:39 +0000

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['660506848136995'], ENV['01bd6a3de70551957289923b52b1701d']
 end 
